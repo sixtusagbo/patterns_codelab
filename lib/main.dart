a2958281a90f2c6b82e9f6bf66ac951516d63ad5
Sixtus Agbo <miracleagbosixtus@gmail.com>
2023-07-12 16:35:24 +0100

    Create project

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: const Scaffold(
+        body: Center(
+          child: Text('Hello World!'),
+        ),
+      ),
+      theme: ThemeData(useMaterial3: true),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
