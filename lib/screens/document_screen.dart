1d3b88a7c4453c7a760a4c51fdb4d8985860d2fd
Sixtus Agbo <miracleagbosixtus@gmail.com>
2023-07-13 00:24:31 +0100

    Use object patterns to display relative dates

--- lib/screens/document_screen.dart
+++ lib/screens/document_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:patterns_codelab/data.dart';
 import 'package:patterns_codelab/screens/components/block_widget.dart';
+import 'package:patterns_codelab/util.dart';
 
 class DocumentScreen extends StatelessWidget {
   final Document document;
@@ -21,7 +22,7 @@ class DocumentScreen extends StatelessWidget {
       ),
       body: Column(
         children: [
-          Text('Last modified $modified'),
+          Text('Last modified ${formatDate(modified)}'),
           Expanded(
             child: ListView.builder(
               itemCount: blocks.length,
---
+++ lib/util.dart
@@ -0,0 +1,15 @@
+String formatDate(DateTime dateTime) {
+  final today = DateTime.now();
+  final difference = dateTime.difference(today);
+
+  return switch (difference) {
+    Duration(inDays: 0) => 'today',
+    Duration(inDays: 1) => 'tomorrow',
+    Duration(inDays: -1) => 'yesterday',
+    Duration(inDays: final days) when days > 7 => '${days ~/ 7} weeks from now',
+    Duration(inDays: final days) when days < -7 =>
+      '${days.abs() ~/ 7} weeks ago',
+    Duration(inDays: final days, isNegative: true) => '${days.abs()} days ago',
+    Duration(inDays: final days) => '$days days from now'
+  };
+}
